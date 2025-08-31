1b3ff9528d6426c76acb40e15f250510a07c9f93
Natãn Schneider <natanschneider@protonmail.com>
2025-08-31 19:15:41 -0300

    *feat* create model for applications

---
+++ app/Models/applications.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+final class applications extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'status',
+    ];
+}
