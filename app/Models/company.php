41a7d5b421c8c2f62bf38981eaa9bdc1dc9565f9
Natãn Schneider <natanschneider@protonmail.com>
2025-08-31 18:55:31 -0300

    *feat* create company model

---
+++ app/Models/company.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+final class company extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'status',
+    ];
+}
