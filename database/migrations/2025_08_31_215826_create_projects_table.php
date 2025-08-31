24d629ffe055ec1b481e96fe880572fe33708685
Natãn Schneider <natanschneider@protonmail.com>
2025-08-31 19:15:02 -0300

    *feat* creat migration for table projects

---
+++ database/migrations/2025_08_31_215826_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Ramsey\Uuid\Uuid;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table): void {
+            $table->id();
+            $table->string('name', 255);
+            $table->unsignedBigInteger('company_id');
+            $table->uuid('uuid')->default(Uuid::uuid4());
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+
+            $table->foreign('company_id')->references('id')->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
