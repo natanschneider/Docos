9131f5669bc78a3c95036b39c78e23879841406d
Natãn Schneider <natanschneider@protonmail.com>
2025-09-01 12:06:17 -0300

    *fix* add strict types and final class

--- app/Models/columns.php
+++ app/Models/columns.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class columns extends Model
+final class columns extends Model
 {
     //
 }
