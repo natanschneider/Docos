514ba3ba30647a868a7e23da6142f1929ad40e5c
Natãn Schneider <natanschneider@protonmail.com>
2025-09-01 11:28:52 -0300

    *feat* create model for table tables

---
+++ database/migrations/2025_09_01_135211_create_tables_table.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Ramsey\Uuid\Uuid;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tables', function (Blueprint $table): void {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('doc_file', 500)->nullable();
+            $table->unsignedBigInteger('database_id');
+            $table->uuid('uuid')->default(Uuid::uuid4());
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+
+            $table->foreign('database_id')->references('id')->on('databases');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tables');
+    }
+};
