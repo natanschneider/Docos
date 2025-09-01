69d9ca176da1e7d71bc3c118c3ea3adadec66a60
Natãn Schneider <natanschneider@protonmail.com>
2025-09-01 12:12:22 -0300

    *feat* create model for table screens

---
+++ database/migrations/2025_09_01_145810_create_screens_table.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Ramsey\Uuid\Uuid;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('screens', function (Blueprint $table): void {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('doc_file', 500)->nullable();
+            $table->unsignedBigInteger('application_id');
+            $table->uuid('uuid')->default(Uuid::uuid4());
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+
+            $table->foreign('application_id')->references('id')->on('applications');
+        });
+
+        Schema::create('screen_tables', function (Blueprint $table): void {
+            $table->id();
+            $table->unsignedBigInteger('screen_id');
+            $table->unsignedBigInteger('table_id');
+
+            $table->foreign('screen_id')->references('id')->on('screens');
+            $table->foreign('table_id')->references('id')->on('tables');
+        });
+
+        Schema::create('screen_columns', function (Blueprint $table): void {
+            $table->id();
+            $table->unsignedBigInteger('screen_id');
+            $table->unsignedBigInteger('column_id');
+
+            $table->foreign('screen_id')->references('id')->on('screens');
+            $table->foreign('column_id')->references('id')->on('columns');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('screens');
+        Schema::dropIfExists('screen_tables');
+        Schema::dropIfExists('screen_columns');
+    }
+};
