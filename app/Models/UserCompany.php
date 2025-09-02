f1e42a8167aaeb845fa4be432085ae995b025726
Natãn Schneider <natanschneider@protonmail.com>
2025-09-02 14:28:51 -0300

    *feat* add no timestamp on table and fillable columns

--- app/Models/UserCompany.php
+++ app/Models/UserCompany.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -7,9 +9,26 @@ use Illuminate\Database\Eloquent\Model;
 class UserCompany extends Model
 {
     /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
      * The table associated with the model.
      *
      * @var string
      */
     protected $table = 'user_company';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'company_id',
+    ];
 }
