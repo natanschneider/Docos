a2d4a25c234bd2dd1ff382f43a33d47d44fc1990
Natãn Schneider <natanschneider@protonmail.com>
2025-09-03 10:04:01 -0300

    *feat* receive request to get companies

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -7,11 +7,17 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CompanyRequest;
 use App\Models\Company;
 use App\Repositories\CompanyRepository;
+use Illuminate\Database\Eloquent\Collection;
 
 final class CompanyController extends Controller
 {
     public function store(CompanyRequest $request): Company
     {
         return (new CompanyRepository())->create($request);
+    }
+
+    public function get(CompanyRequest $request): Collection
+    {
+        return (new CompanyRepository())->get($request);
     }
 }
