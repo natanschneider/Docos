323856be94b81c8f9e5b45fc8299444fa95d2801
Natãn Schneider <natanschneider@protonmail.com>
2025-09-04 09:03:45 -0300

    *feat* receive and handle post request to project

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProjectRequest;
+use App\Models\Project;
+use App\Repositories\ProjectRepository;
+
+class ProjectController extends Controller
+{
+    public function store(ProjectRequest $request): Project
+    {
+        (new ProjectRequest())->authorize($request);
+
+        return (new ProjectRepository())->create($request);
+    }
+}
