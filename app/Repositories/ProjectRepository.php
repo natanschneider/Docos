bdccd9a52bc4cd068b098f28bc4b6a68ed2832d5
Natãn Schneider <natanschneider@protonmail.com>
2025-09-05 14:49:33 -0300

    *feat* get and update resources DB

--- app/Repositories/ProjectRepository.php
+++ app/Repositories/ProjectRepository.php
@@ -6,6 +6,7 @@ namespace App\Repositories;
 
 use App\Http\Requests\ProjectRequest;
 use App\Models\Project;
+use Illuminate\Database\Eloquent\Collection;
 
 final class ProjectRepository
 {
@@ -15,5 +16,31 @@ final class ProjectRepository
             'name' => $request->name,
             'company_id' => $request->company_id
         ]);
+    }
+
+    public function get(ProjectRequest $request): Collection
+    {
+        $query = Project::query();
+
+        if ($request->has('id')) {
+            $query->where('id', $request->id);
+        }
+
+        if ($request->has('company_id')) {
+            $query->where('company_id', $request->company_id);
+        }
+
+        return $query->get();
+    }
+
+    public function update(ProjectRequest $request): Collection
+    {
+        if ($request->has('company_id') && $request->user()->companies()->where('companies.id', $request->company_id)->doesntExist()) {
+            abort(403, 'Provided company does not belong to user');
+        }
+
+        Project::where('id', $request->id)->update($request->all());
+
+        return Project::where('id', $request->id)->get();
     }
 }
