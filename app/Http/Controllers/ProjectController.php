5d1d3693897fce056217e9d31a376e7dc481e420
Natãn Schneider <natanschneider@protonmail.com>
2025-09-05 14:54:49 -0300

    *feat* receive and handle request to get and update resources

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -7,11 +7,28 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ProjectRequest;
 use App\Models\Project;
 use App\Repositories\ProjectRepository;
+use Illuminate\Database\Eloquent\Collection;
 
 class ProjectController extends Controller
 {
     public function store(ProjectRequest $request): Project
     {
         return (new ProjectRepository())->create($request);
+    }
+
+    public function get(ProjectRequest $request): Collection
+    {
+        if ($request->has('id')) {
+            (new ProjectRequest())->ensureProjectBelongsToUser($request);
+        }
+
+        return (new ProjectRepository())->get($request);
+    }
+
+    public function update(ProjectRequest $request): Collection
+    {
+        (new ProjectRequest())->ensureProjectBelongsToUser($request);
+
+        return (new ProjectRepository())->update($request);
     }
 }
