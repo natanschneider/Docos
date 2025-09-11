8829cae39e4047e67c89cdb5a5dc679ad186f28a
Natãn Schneider <natanschneider@protonmail.com>
2025-09-11 15:03:06 -0300

    *feat* add request rules for POST method

---
+++ app/Http/Requests/ScreenRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ScreenRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return match ($this->method()) {
+            'POST' => [
+                'name' => ['required', 'string', 'max:255'],
+                'application_id' => ['required', 'string', 'exists:applications,id'],
+            ],
+            'DEFAULT' => [
+                'id' => ['string', 'exists:screens,id'],
+                'name' => ['string', 'max:255'],
+                'doc_file' => ['string', 'max:500'],
+                'application_id' => ['string', 'exists:applications,id'],
+                'uuid' => ['uuid', 'exists:screens,uuid'],
+                'status' => ['boolean'],
+            ]
+        };
+    }
+}
