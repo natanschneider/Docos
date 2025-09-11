4bc9cebe19431a23c172b85897fa33bda7a15f87
Natãn Schneider <natanschneider@protonmail.com>
2025-09-11 09:25:47 -0300

    *feat* declare relationship between applications and screens

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 final class Application extends Model
 {
@@ -34,5 +35,13 @@ final class Application extends Model
     public function endpoints(): HasMany
     {
         return $this->hasMany(Endpoint::class);
+    }
+
+    /**
+     * Get the application's screens
+     */
+    public function screens(): HasMany
+    {
+        return $this->hasMany(Screen::class);
     }
 }
