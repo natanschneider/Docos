da2d2c0d7c76e4efb2c3d09954fbd30951e2c186
Natãn Schneider <natanschneider@protonmail.com>
2025-09-11 16:07:32 -0300

    *feat* handle request to get Endpoint

--- app/Http/Controllers/EndpointController.php
+++ app/Http/Controllers/EndpointController.php
@@ -7,11 +7,21 @@ namespace App\Http\Controllers;
 use App\Http\Requests\EndpointRequest;
 use App\Models\Endpoint;
 use App\Repositories\EndpointRepository;
+use Illuminate\Database\Eloquent\Collection;
 
 class EndpointController extends Controller
 {
     public function store(EndpointRequest $request): Endpoint
     {
         return (new EndpointRepository())->create($request);
+    }
+
+    public function get(EndpointRequest $request): Collection
+    {
+        if ($request->has('id')) {
+            (new EndpointRequest())->ensureEndpointBelongsToUser($request);
+        }
+
+        return (new EndpointRepository())->get($request);
     }
 }
