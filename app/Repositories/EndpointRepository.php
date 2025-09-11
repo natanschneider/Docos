206a6ec3fd40e9d8902f1301d47145d7e3c6a746
Natãn Schneider <natanschneider@protonmail.com>
2025-09-11 16:03:13 -0300

    *feat* get Endpoint resource at DB

--- app/Repositories/EndpointRepository.php
+++ app/Repositories/EndpointRepository.php
@@ -6,11 +6,27 @@ namespace App\Repositories;
 
 use App\Http\Requests\EndpointRequest;
 use App\Models\Endpoint;
+use Illuminate\Database\Eloquent\Collection;
 
 final class EndpointRepository
 {
     public function create(EndpointRequest $request): Endpoint
     {
         return Endpoint::create($request->all());
+    }
+
+    public function get(EndpointRequest $request): Collection
+    {
+        $query = Endpoint::query();
+
+        if ($request->has('id')) {
+            $query->where('id', $request->id);
+        }
+
+        if ($request->has('application_id')) {
+            $query->where('application_id', $request->application_id);
+        }
+
+        return $query->get();
     }
 }
