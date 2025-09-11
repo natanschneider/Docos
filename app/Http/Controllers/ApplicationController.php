9dede8e38b006422121b40843822247cf7738306
Natãn Schneider <natanschneider@protonmail.com>
2025-09-11 10:38:53 -0300

    *feat* handle request to retrieve applications

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -7,11 +7,21 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ApplicationRequest;
 use App\Models\Application;
 use App\Repositories\ApplicationRepository;
+use Illuminate\Database\Eloquent\Collection;
 
 class ApplicationController extends Controller
 {
     public function store(ApplicationRequest $request): Application
     {
         return (new ApplicationRepository())->create($request);
+    }
+
+    public function get(ApplicationRequest $request): Collection
+    {
+        if ($request->has('id')) {
+            (new ApplicationRequest())->ensureApplicationBelongsToUser($request);
+        }
+
+        return (new ApplicationRepository())->get($request);
     }
 }
