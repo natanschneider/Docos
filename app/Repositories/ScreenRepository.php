2261a1bcf726b91a78d33133a33b73e85ee8e523
Natãn Schneider <natanschneider@protonmail.com>
2025-09-11 15:12:19 -0300

    *feat* get Screen resource at DB

--- app/Repositories/ScreenRepository.php
+++ app/Repositories/ScreenRepository.php
@@ -6,11 +6,27 @@ namespace App\Repositories;
 
 use App\Http\Requests\ScreenRequest;
 use App\Models\Screen;
+use Illuminate\Database\Eloquent\Collection;
 
 final class ScreenRepository
 {
     public function create(ScreenRequest $request): Screen
     {
         return Screen::create($request->all());
+    }
+
+    public function get(ScreenRequest $request): Collection
+    {
+        $query = Screen::query();
+
+        if ($request->has('id')) {
+            $query->where('id', $request->id);
+        }
+
+        if ($request->has('application_id')) {
+            $query->where('application_id', $request->application_id);
+        }
+
+        return $query->get();
     }
 }
