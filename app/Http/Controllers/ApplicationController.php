750b794e32d4b3377eb04658379bef78012078be
Natãn Schneider <natanschneider@protonmail.com>
2025-09-11 10:13:05 -0300

    *fix* import necessary classes

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApplicationRequest;
+use App\Models\Application;
+use App\Repositories\ApplicationRepository;
+
 class ApplicationController extends Controller
 {
     public function store(ApplicationRequest $request): Application
