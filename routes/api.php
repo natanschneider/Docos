ecbe085fe70e1d714617b928671e1175a5c0af18
Natãn Schneider <natanschneider@protonmail.com>
2025-09-11 15:36:24 -0300

    *feat* route to update Screen

--- routes/api.php
+++ routes/api.php
@@ -41,5 +41,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::controller(ScreenController::class)->group(function () {
         Route::post('screen', 'store');
         Route::get('screen', 'get');
+        Route::put('screen', 'update');
     });
 });
