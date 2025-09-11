767ebb83f40ceec1838c85d878c5c81fdf33bafe
Natãn Schneider <natanschneider@protonmail.com>
2025-09-11 09:26:41 -0300

    *feat* fillable columns in screen model

--- app/Models/Screen.php
+++ app/Models/Screen.php
@@ -10,6 +10,18 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 final class Screen extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'status',
+        'doc_file',
+        'application_id',
+    ];
+
+    /**
      * Get the screen's application
      */
     public function application(): BelongsTo
