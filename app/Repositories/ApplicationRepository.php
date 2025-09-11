8ca6b6b2fa4e6036fdc479abba13c341fb2149ec
Natãn Schneider <natanschneider@protonmail.com>
2025-09-11 10:34:42 -0300

    *feat* get resource from DB

--- app/Repositories/ApplicationRepository.php
+++ app/Repositories/ApplicationRepository.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
+use App\Models\Application;
+use Illuminate\Database\Eloquent\Collection;
+
 final class ApplicationRepository
 {
     public function create(ApplicationRequest $request): Application
@@ -12,5 +15,20 @@ final class ApplicationRepository
             'name' => $request->name,
             'project_id' => $request->project_id,
         ]);
+    }
+
+    public function get(ApplicationRequest $request): Collection
+    {
+        $query = Application::query();
+
+        if ($request->has('id')) {
+            $query->where('id', $request->id);
+        }
+
+        if ($request->has('project_id')) {
+            $query->where('project_id', $request->project_id);
+        }
+
+        return $query->get();
     }
 }
