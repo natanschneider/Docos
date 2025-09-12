54de60d9e360a51254a112b0fa92c6247c3b2b08
Natãn Schneider <natanschneider@protonmail.com>
2025-09-12 11:46:38 -0300

    *feat* declare request rules for POST method

--- app/Http/Requests/TableRequest.php
+++ app/Http/Requests/TableRequest.php
@@ -23,8 +23,20 @@ class TableRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            //
-        ];
+        return match ($this->method()) {
+            'POST' => [
+                'name' => ['required', 'string', 'max:255'],
+                'doc_file' => ['string', 'max:500'],
+                'database_id' => ['required', 'string', 'exists:databases,id'],
+            ],
+            'DEFAULT' => [
+                'id' => ['string', 'exists:tables,id'],
+                'name' => ['string', 'max:255'],
+                'doc_file' => ['string', 'max:500'],
+                'database_id' => ['string', 'exists:databases,id'],
+                'uuid' => ['uuid', 'exists:tables,uuid'],
+                'status' => ['boolean'],
+            ]
+        };
     }
 }
