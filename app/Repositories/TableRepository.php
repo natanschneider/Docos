0b57648bc267dd4ed50ae6b811f41c247decf491
Natãn Schneider <natanschneider@protonmail.com>
2025-09-12 11:47:36 -0300

    *feat* create Table resource at DB

--- app/Repositories/TableRepository.php
+++ app/Repositories/TableRepository.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
+use App\Http\Requests\TableRequest;
+use App\Models\Table;
+
 final class TableRepository
 {
-    //
+    public function create(TableRequest $request): Table
+    {
+        return Table::create($request->all());
+    }
 }
