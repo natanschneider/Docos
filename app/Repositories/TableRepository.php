66b0c628ed5760c1983ecc1a56f049da7740bb87
Natãn Schneider <natanschneider@protonmail.com>
2025-09-12 11:52:42 -0300

    *feat* get Table resource at DB

--- app/Repositories/TableRepository.php
+++ app/Repositories/TableRepository.php
@@ -6,11 +6,27 @@ namespace App\Repositories;
 
 use App\Http\Requests\TableRequest;
 use App\Models\Table;
+use Illuminate\Database\Eloquent\Collection;
 
 final class TableRepository
 {
     public function create(TableRequest $request): Table
     {
         return Table::create($request->all());
+    }
+
+    public function get(TableRequest $request): Collection
+    {
+        $query = Table::query();
+
+        if ($request->has('id')) {
+            $query->where('id', $request->id);
+        }
+
+        if ($request->has('database_id')) {
+            $query->where('database_id', $request->database_id);
+        }
+
+        return $query->get();
     }
 }
