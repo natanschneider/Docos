4fcc6fcee6f9a8d7a15dcf8f9065d86590bf2d68
Natãn Schneider <natanschneider@protonmail.com>
2025-09-12 13:43:54 -0300

    *feat* handle request to get Column

--- app/Http/Controllers/ColumnController.php
+++ app/Http/Controllers/ColumnController.php
@@ -7,11 +7,21 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ColumnRequest;
 use App\Models\Column;
 use App\Repositories\ColumnRepository;
+use Illuminate\Database\Eloquent\Collection;
 
 class ColumnController extends Controller
 {
     public function store(ColumnRequest $request): Column
     {
         return (new ColumnRepository())->create($request);
+    }
+
+    public function get(ColumnRequest $request): Collection
+    {
+        if ($request->has('id')) {
+            (new ColumnRequest())->unsureColumnBelongsToUser($request);
+        }
+
+        return (new ColumnRepository())->get($request);
     }
 }
