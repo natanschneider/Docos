caeaf8d053b641154eb80f9fd28e8f70fdca0d39
Natãn Schneider <natanschneider@protonmail.com>
2025-09-12 09:56:56 -0300

    *fix* import necessary class

--- app/Models/Database.php
+++ app/Models/Database.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 final class Database extends Model
 {
