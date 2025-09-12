bb0b9158deefa2545de7621902775dd270d7e016
Natãn Schneider <natanschneider@protonmail.com>
2025-09-12 11:48:36 -0300

    *feat* handle request to create Table

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TableRequest;
+use App\Models\Table;
+use App\Repositories\TableRepository;
+
 class TableController extends Controller
 {
-    //
+    public function store(TableRequest $request): Table
+    {
+        return (new TableRepository())->create($request);
+    }
 }
