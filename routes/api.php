282f73a7c096d4b1176404a554c6791f912c6a77
Natãn Schneider <natanschneider@protonmail.com>
2025-09-12 11:57:17 -0300

    *feat* route to update Table

--- routes/api.php
+++ routes/api.php
@@ -65,5 +65,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::controller(TableController::class)->group(function () {
         Route::post('table', 'store');
         Route::get('table', 'get');
+        Route::put('table', 'update');
     });
 });
