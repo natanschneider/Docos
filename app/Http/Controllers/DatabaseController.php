676e5d5aeccb5242ccbfef18a5a4a481c15f9a0c
Natãn Schneider <natanschneider@protonmail.com>
2025-09-12 10:15:26 -0300

    *feat* handle request to get Database

--- app/Http/Controllers/DatabaseController.php
+++ app/Http/Controllers/DatabaseController.php
@@ -7,11 +7,21 @@ namespace App\Http\Controllers;
 use App\Http\Requests\DatabaseRequest;
 use App\Models\Database;
 use App\Repositories\DatabaseRepository;
+use Illuminate\Database\Eloquent\Collection;
 
 class DatabaseController extends Controller
 {
     public function store(DatabaseRequest $request): Database
     {
         return (new DatabaseRepository())->create($request);
+    }
+
+    public function get(DatabaseRequest $request): Collection
+    {
+        if ($request->has('id')) {
+            (new DatabaseRequest())->ensureDatabaseBelongsToUser($request);
+        }
+
+        return (new DatabaseRepository())->get($request);
     }
 }
