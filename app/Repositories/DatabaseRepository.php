1f9b9fc4c800ba01d372f7d48d4bf881fd52572d
Natãn Schneider <natanschneider@protonmail.com>
2025-09-12 10:15:02 -0300

    *feat* get Database resource at DB

--- app/Repositories/DatabaseRepository.php
+++ app/Repositories/DatabaseRepository.php
@@ -6,11 +6,27 @@ namespace App\Repositories;
 
 use App\Http\Requests\DatabaseRequest;
 use App\Models\Database;
+use Illuminate\Database\Eloquent\Collection;
 
 final class DatabaseRepository
 {
     public function create(DatabaseRequest $request): Database
     {
         return Database::create($request->all());
+    }
+
+    public function get(DatabaseRequest $request): Collection
+    {
+        $query = Database::query();
+
+        if ($request->has('id')) {
+            $query->where('id', $request->id);
+        }
+
+        if ($request->has('company_id')) {
+            $query->where('company_id', $request->company_id);
+        }
+
+        return $query->get();
     }
 }
