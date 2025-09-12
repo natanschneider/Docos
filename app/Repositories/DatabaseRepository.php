e71e07ca4d840c133d49792ae1ba676763146bdc
Natãn Schneider <natanschneider@protonmail.com>
2025-09-12 10:11:13 -0300

    *feat* create Database resource at DB

--- app/Repositories/DatabaseRepository.php
+++ app/Repositories/DatabaseRepository.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
+use App\Http\Requests\DatabaseRequest;
+use App\Models\Database;
+
 final class DatabaseRepository
 {
+    public function create(DatabaseRequest $request): Database
+    {
+        return Database::create($request->all());
+    }
 }
