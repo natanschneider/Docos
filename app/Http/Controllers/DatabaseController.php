c5b5229a1e0db9c13058f31e81f3c7c15c85e18c
Natãn Schneider <natanschneider@protonmail.com>
2025-09-12 10:12:26 -0300

    *feat* handle request to create Database

--- app/Http/Controllers/DatabaseController.php
+++ app/Http/Controllers/DatabaseController.php
@@ -4,6 +4,14 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\DatabaseRequest;
+use App\Models\Database;
+use App\Repositories\DatabaseRepository;
+
 class DatabaseController extends Controller
 {
+    public function store(DatabaseRequest $request): Database
+    {
+        return (new DatabaseRepository())->create($request);
+    }
 }
