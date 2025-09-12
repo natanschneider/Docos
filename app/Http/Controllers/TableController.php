1bc4553b0107ed8ba623f3076ed4fe50608d9893
Natãn Schneider <natanschneider@protonmail.com>
2025-09-12 11:53:16 -0300

    *feat* handle request to get Table

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -7,11 +7,21 @@ namespace App\Http\Controllers;
 use App\Http\Requests\TableRequest;
 use App\Models\Table;
 use App\Repositories\TableRepository;
+use Illuminate\Database\Eloquent\Collection;
 
 class TableController extends Controller
 {
     public function store(TableRequest $request): Table
     {
         return (new TableRepository())->create($request);
+    }
+
+    public function get(TableRequest $request): Collection
+    {
+        if ($request->has('id')) {
+            (new TableRequest())->ensureTableBelongsToUser($request);
+        }
+
+        return (new TableRepository())->get($request);
     }
 }
