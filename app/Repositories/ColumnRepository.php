cb606bcc73e81a595de30c74a503ec1ad3b61d25
Natãn Schneider <natanschneider@protonmail.com>
2025-09-12 13:43:26 -0300

    *feat* get Column resource at DB

--- app/Repositories/ColumnRepository.php
+++ app/Repositories/ColumnRepository.php
@@ -6,11 +6,27 @@ namespace App\Repositories;
 
 use App\Http\Requests\ColumnRequest;
 use App\Models\Column;
+use Illuminate\Database\Eloquent\Collection;
 
 final class ColumnRepository
 {
     public function create(ColumnRequest $request): Column
     {
         return Column::create($request->all());
+    }
+
+    public function get(ColumnRequest $request): Collection
+    {
+        $query = Column::query();
+
+        if ($request->has('id')) {
+            $query->where('id', $request->id);
+        }
+
+        if ($request->has('table_id')) {
+            $query->where('table_id', $request->table_id);
+        }
+
+        return $query->get();
     }
 }
