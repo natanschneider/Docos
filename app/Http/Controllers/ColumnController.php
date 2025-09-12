fc94521606abc5f9e66aebace935e0980ecfbc43
Natãn Schneider <natanschneider@protonmail.com>
2025-09-12 13:40:53 -0300

    *feat* handle request to create Column

--- app/Http/Controllers/ColumnController.php
+++ app/Http/Controllers/ColumnController.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ColumnRequest;
+use App\Models\Column;
+use App\Repositories\ColumnRepository;
+
 class ColumnController extends Controller
 {
-    //
+    public function store(ColumnRequest $request): Column
+    {
+        return (new ColumnRepository())->create($request);
+    }
 }
