8654df551bb60649deaefcc26f73be0809f8794d
Natãn Schneider <natanschneider@protonmail.com>
2025-09-12 13:42:05 -0300

    *feat* declare request rules for GET method

--- app/Http/Requests/ColumnRequest.php
+++ app/Http/Requests/ColumnRequest.php
@@ -30,6 +30,10 @@ class ColumnRequest extends FormRequest
                 'table_id' => ['required', 'string', 'exists:tables,id'],
                 'type_id' => ['required', 'string', 'exists:types,id'],
             ],
+            'GET' => [
+                'id' => ['required_if:table_id,null', 'string', 'exists:columns,id'],
+                'table_id' => ['required_if:id,null', 'string', 'exists:tables,id'],
+            ],
             'DEFAULT' => [
                 'id' => ['string', 'exists:columns,id'],
                 'name' => ['string', 'max:255'],
