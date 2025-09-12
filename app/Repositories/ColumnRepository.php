c856d6272f793c114d25071a636d761c8e71e9fd
Natãn Schneider <natanschneider@protonmail.com>
2025-09-12 13:40:06 -0300

    *feat* create Column resource at DB

--- app/Repositories/ColumnRepository.php
+++ app/Repositories/ColumnRepository.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
+use App\Http\Requests\ColumnRequest;
+use App\Models\Column;
+
 final class ColumnRepository
 {
-    //
+    public function create(ColumnRequest $request): Column
+    {
+        return Column::create($request->all());
+    }
 }
