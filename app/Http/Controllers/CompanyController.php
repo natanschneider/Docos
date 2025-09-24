3748bbb9669ab0e9f8df5e190e76a8d1b852c184
Natãn Schneider <natanschneider@protonmail.com>
2025-09-24 16:07:19 -0300

    *feat* call policies in Company controller

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\CompanyRequest;
 use App\Models\Company;
+use Illuminate\Support\Facades\Gate;
+use App\Http\Requests\CompanyRequest;
 use App\Repositories\CompanyRepository;
 use Illuminate\Database\Eloquent\Collection;
 
@@ -13,25 +14,29 @@ final class CompanyController extends Controller
 {
     public function store(CompanyRequest $request): Company
     {
+        Gate::authorize('create', Company::class);
         return (new CompanyRepository())->create($request);
     }
 
     public function get(CompanyRequest $request): Collection
     {
+        if ($request->has('id')) {
+            Gate::authorize('view', Company::findOrFail($request->id));
+        }
+
         return (new CompanyRepository())->get($request);
     }
 
     public function update(CompanyRequest $request): Collection
     {
-        (new CompanyRequest())->ensureCompanyBelongsToUser($request);
+        Gate::authorize('update', Company::findOrFail($request->id));
 
         return (new CompanyRepository())->update($request);
     }
 
     public function destroy(CompanyRequest $request): Company|Collection
     {
-        (new CompanyRequest())->ensureCompanyBelongsToUser($request);
-        (new CompanyRequest())->ensureCompanyIsEmpty($request);
+        Gate::authorize('delete', Company::findOrFail($request->id));
 
         return (new CompanyRepository())->delete($request);
     }
