9a04b9f6481ca2401fd84fa967c4ca9d899adc7b
Natãn Schneider <natanschneider@protonmail.com>
2025-09-25 17:12:21 -0300

    *feat* add policies to Project requests

---
+++ app/Policies/ProjectPolicy.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies;
+
+use App\Http\Requests\ProjectRequest;
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class ProjectPolicy
+{
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Project $project, ProjectRequest $request): Response
+    {
+        if (
+            $request->has('company_id') &&
+            $user->companies()->where('companies.id', $request->company_id)->doesntExist()
+        ) {
+            return Response::deny('Company provided does not belong to user or does not exist');
+        }
+
+        if (
+            $request->has('id') &&
+            $user->companies()->where(
+                'companies.id',
+                Project::findOrFail($request->id)->company_id
+            )->doesntExist()
+        ) {
+            return Response::deny('Project provided does not belong to user or does not exist');
+        }
+
+        return Response::allow();
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user, ProjectRequest $request): Response
+    {
+        return $user->companies()->where('companies.id', $request->company_id)->exists()
+            ? Response::allow()
+            : Response::deny('Company provided does not belong to user or does not exist');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Project $project): Response
+    {
+        return $user->companies()->where('companies.id', $project->company_id)->exists()
+            ? Response::allow()
+            : Response::deny('Project does not belong to user or does not exist');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Project $project): Response
+    {
+        if ($user->companies()->where('companies.id', $project->company_id)->doesntExist()) {
+            return Response::deny('Project does not belong to user or does not exist');
+        }
+
+        return $project->applications()->doesntExist()
+            ? Response::allow()
+            : Response::deny('Project provided contains applications');
+    }
+}
