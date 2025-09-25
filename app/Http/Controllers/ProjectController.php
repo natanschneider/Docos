46462499e1e389be81fb2df8e8c087b9befe2cbd
Natãn Schneider <natanschneider@protonmail.com>
2025-09-25 17:17:54 -0300

    *feat* use Policies through gates

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -8,35 +8,34 @@ use App\Http\Requests\ProjectRequest;
 use App\Models\Project;
 use App\Repositories\ProjectRepository;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Gate;
 
 class ProjectController extends Controller
 {
     public function store(ProjectRequest $request): Project
     {
+        Gate::authorize('create', [Project::class, $request]);
+
         return (new ProjectRepository())->create($request);
     }
 
     public function get(ProjectRequest $request): Collection
     {
-        if ($request->has('id')) {
-            (new ProjectRequest())->ensureProjectBelongsToUser($request);
-        }
+        Gate::authorize('view', [Project::class, $request]);
 
         return (new ProjectRepository())->get($request);
     }
 
     public function update(ProjectRequest $request): Collection
     {
-        (new ProjectRequest())->ensureProjectBelongsToUser($request);
+        Gate::authorize('update', [Project::findOrFail($request->id), $request]);
 
         return (new ProjectRepository())->update($request);
     }
 
     public function destroy(ProjectRequest $request): Project|Collection
     {
-        $formRequest = new ProjectRequest();
-        $formRequest->ensureProjectBelongsToUser($request);
-        $formRequest->ensureProjectIsEmpty($request);
+        Gate::authorize('delete', [Project::findOrFail($request->id), $request]);
 
         return (new ProjectRepository())->destroy($request);
     }
