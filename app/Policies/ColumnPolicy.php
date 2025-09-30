25f7e241f0f9e8d7529b1a6510d1a3fac6f97d1f
Natãn Schneider <natanschneider@protonmail.com>
2025-09-30 17:09:14 -0300

    *feat* create policies for Column resource

---
+++ app/Policies/ColumnPolicy.php
@@ -0,0 +1,127 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies;
+
+use App\Http\Requests\ColumnRequest;
+use App\Models\Column;
+use App\Models\Table;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class ColumnPolicy
+{
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, ColumnRequest $request): Response
+    {
+        if (
+            $request->has('table_id') &&
+            $user->companies()
+                ->where(
+                    'company_id',
+                    Table::findOrFail($request->table_id)->database->company_id
+                )
+                ->doesntExist()
+        ) {
+            return Response::deny('Table provided does not belong to user or does not exist');
+        }
+
+        if (
+            $request->has('id') &&
+            $user->companies()
+                ->where(
+                    'company_id',
+                    Column::findOrFail($request->id)->table->database->company_id
+                )
+                ->doesntExist()
+        ) {
+            return Response::deny('Column provided does not belong to user or does not exist');
+        }
+
+        return Response::allow();
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user, ColumnRequest $request): Response
+    {
+        if (
+            $request->has('table_id') &&
+            $user->companies()
+                ->where(
+                    'company_id',
+                    Table::findOrFail($request->table_id)->database->company_id
+                )
+                ->doesntExist()
+        ) {
+            return Response::deny('Table provided does not belong to user or does not exist');
+        }
+
+        // TODO: verify if columns to attach, exists, belong to same database and have a PK/FK constraint
+        if ($request->has('related_columns')) {
+            if (isset($request->related_columns['pk'])) {
+                $pk_counter = count($request->related_columns['pk']);
+            }
+
+            if (isset($request->related_columns['fk'])) {
+                $fk_counter = count($request->related_columns['fk']);
+            }
+        }
+
+        return Response::allow();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Column $column, ColumnRequest $request): Response
+    {
+        if ($user->companies()->where('companies.id', $column->table->database->company_id)->doesntExist()) {
+            return Response::deny('Column does not belong to user or does not exist');
+        }
+
+        if (
+            $request->has('table_id') &&
+            Table::findOrFail($request->table_id)->database->company_id !== $column->table->database->company_id
+        ) {
+            return Response::deny('Table provided does not belong to same company as column');
+        }
+
+        // TODO: verify if columns to attach, exists, belong to same database and have a PK/FK constraint
+        if ($request->has('related_columns')) {
+            if (isset($request->related_columns['pk'])) {
+                $pk_counter = count($request->related_columns['pk']);
+            }
+
+            if (isset($request->related_columns['fk'])) {
+                $fk_counter = count($request->related_columns['fk']);
+            }
+        }
+
+        return Response::allow();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Column $column): Response
+    {
+        if ($user->companies()->where('companies.id', $column->table->database->company_id)->doesntExist()) {
+            return Response::deny('Column does not belong to user or does not exist');
+        }
+
+        if ($column->screens()->exists() || $column->endpoints()->exists()) {
+            Response::deny('Column provided contains endpoints or screens');
+        }
+
+        if ($column->relatedFks()->exists() || $column->relatedPks()->exists()) {
+            Response::deny('Column provided contains related primary keys or foreign keys');
+        }
+
+        return Response::allow();
+    }
+}
