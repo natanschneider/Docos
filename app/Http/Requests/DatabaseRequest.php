f73808edb8e655fe0167b766098765d13f5daba1
Natãn Schneider <natanschneider@protonmail.com>
2025-09-30 10:57:44 -0300

    *remove* policies inside request object

--- app/Http/Requests/DatabaseRequest.php
+++ app/Http/Requests/DatabaseRequest.php
@@ -4,23 +4,10 @@ declare(strict_types=1);
 
 namespace App\Http\Requests;
 
-use App\Models\Database;
 use Illuminate\Foundation\Http\FormRequest;
 
 class DatabaseRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        if (isset($this->company_id) && $this->user()->companies()->where('companies.id', $this->company_id)->doesntExist()) {
-            abort(403, 'Company does not belong to user or does not exist');
-        }
-
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
@@ -41,7 +28,6 @@ class DatabaseRequest extends FormRequest
             'PUT' => [
                 'id' => ['required', 'string', 'exists:databases,id'],
                 'name' => ['string', 'max:255'],
-                'company_id' => ['string', 'exists:companies,id'],
                 'engine_id' => ['string', 'exists:engines,id'],
             ],
             'DELETE' => [
@@ -56,36 +42,5 @@ class DatabaseRequest extends FormRequest
                 'status' => ['boolean'],
             ]
         };
-    }
-
-    /**
-     * Determine if the id provided matches a company that belongs to the user
-     * If id and company_id provided, check if database belongs to given company
-     */
-    public function ensureDatabaseBelongsToUser(self $request): void
-    {
-        $database = Database::where('id', $request->id)->first();
-
-        if ($request->has('company_id') && $request->company_id !== (string) $database->company_id) {
-            abort(403, 'Database does not belong to provided company or does not exist');
-        }
-
-        if ($request->user()->companies()->where('companies.id', $database->company_id)->doesntExist()) {
-            abort(403, 'Database does not belong to user or does not exist');
-        }
-    }
-
-    /**
-     * Determine if the database provided is empty
-     */
-    public function ensureDatabaseIsEmpty(self $request): bool
-    {
-        $database = Database::where('id', $request->id)->first();
-
-        if ($database->tables()->exists()) {
-            abort(403, 'Database contains tables');
-        }
-
-        return true;
     }
 }
