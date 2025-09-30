4a8fa70c5906584d3ee062580511adfed4f4af25
Natãn Schneider <natanschneider@protonmail.com>
2025-09-30 12:16:05 -0300

    *feat* create policies for Table resource

---
+++ app/Policies/TablePolicy.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies;
+
+use App\Http\Requests\TableRequest;
+use App\Models\Database;
+use App\Models\Table;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class TablePolicy
+{
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, TableRequest $request): Response
+    {
+        if (
+            $request->has('database_id') &&
+            $user->companies()
+                ->where(
+                    'company_id',
+                    Database::findOrFail($request->database_id)->company_id
+                )
+                ->doesntExist()
+        ) {
+            return Response::deny('Database provided does not belong to user or does not exist');
+        }
+
+        if (
+            $request->has('id') &&
+            $user->companies()
+                ->where(
+                    'company_id',
+                    Table::findOrFail($request->id)->database->company_id
+                )
+                ->doesntExist()
+        ) {
+            return Response::deny('Table provided does not belong to user or does not exist');
+        }
+
+        return Response::allow();
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user, TableRequest $request): Response
+    {
+        return $user->companies()->where('companies.id', Database::findOrFail($request->database_id)->company_id)->exists()
+            ? Response::allow()
+            : Response::deny('Database provided does not belong to user or does not exist');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Table $table, TableRequest $request): Response
+    {
+        if ($user->companies()->where('companies.id', $table->database->company_id)->doesntExist()) {
+            return Response::deny('Table does not belong to user or does not exist');
+        }
+
+        if (
+            $request->has('database_id') &&
+            $table->database_id !== (int) $request->database_id &&
+            $table->database()->company_id !== Database::findOrFail($request->database_id)->company_id
+        ) {
+            return Response::deny('Database provided does not belong to the same company as the table');
+        }
+
+        return Response::allow();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Table $table): Response
+    {
+        if ($user->companies()->where('companies.id', $table->database->company_id)->doesntExist()) {
+            return Response::deny('Table does not belong to user or does not exist');
+        }
+
+        return $table->columns()->exists()
+            ? Response::deny('Table provided contains columns')
+            : Response::allow();
+    }
+}
