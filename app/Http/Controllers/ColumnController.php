6a5570f95b36d6d04b7530d8888f4663a1f37e83
Natãn Schneider <natanschneider@protonmail.com>
2025-09-30 17:09:26 -0300

    *fix* call policies trough gates

--- app/Http/Controllers/ColumnController.php
+++ app/Http/Controllers/ColumnController.php
@@ -8,34 +8,34 @@ use App\Http\Requests\ColumnRequest;
 use App\Models\Column;
 use App\Repositories\ColumnRepository;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Gate;
 
 class ColumnController extends Controller
 {
     public function store(ColumnRequest $request): Column
     {
+        Gate::authorize('create', [Column::class, $request]);
+
         return (new ColumnRepository())->create($request);
     }
 
     public function get(ColumnRequest $request): Collection
     {
-        if ($request->has('id')) {
-            (new ColumnRequest())->unsureColumnBelongsToUser($request);
-        }
+        Gate::authorize('view', [Column::class, $request]);
 
         return (new ColumnRepository())->get($request);
     }
 
     public function update(ColumnRequest $request): Column
     {
-        (new ColumnRequest())->unsureColumnBelongsToUser($request);
+        Gate::authorize('update', [Column::findOrFail($request->id), $request]);
 
         return (new ColumnRepository())->update($request);
     }
 
     public function destroy(ColumnRequest $request): Column|Collection
     {
-        (new ColumnRequest())->unsureColumnBelongsToUser($request);
-        (new ColumnRequest())->ensureColumnIsEmpty($request);
+        Gate::authorize('delete', Column::findOrFail($request->id));
 
         return (new ColumnRepository())->destroy($request);
     }
