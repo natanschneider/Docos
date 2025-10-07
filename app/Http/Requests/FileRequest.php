4d529f962eca5e7d1628336963b21d5eaad87359
Natãn Schneider <natanschneider@protonmail.com>
2025-10-07 10:59:06 -0300

    *feat* validate file requests

---
+++ app/Http/Requests/FileRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FileRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id' => ['required', 'string|integer'],
+            'file' => [
+                'required',
+                'file',
+                function ($attribute, $value, $fail): void {
+                    $mimeType = $value->getMimeType();
+                    $extension = $value->getClientOriginalExtension();
+
+                    $acceptableMimeTypes = [
+                        'text/markdown',
+                        'text/x-markdown',
+                        'application/x-markdown',
+                        'text/plain',
+                    ];
+
+                    $acceptableExtensions = ['md', 'markdown'];
+
+                    if (! in_array($mimeType, $acceptableMimeTypes)) {
+                        $fail("The {$attribute} must be a markdown file. Detected MIME type: {$mimeType}");
+                    }
+
+                    if (! in_array(mb_strtolower($extension), $acceptableExtensions)) {
+                        $fail("The {$attribute} must have an extension of .md");
+                    }
+                },
+                'max:10240',
+            ],
+        ];
+    }
+}
