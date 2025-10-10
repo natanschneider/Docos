3338f0f699ee945eb45a1dd97a8518737d5a9fbb
Natãn Schneider <natanschneider@protonmail.com>
2025-10-10 14:45:17 -0300

    *feat* parameter to control labels visibility

--- resources/js/components/appearance-tabs.tsx
+++ resources/js/components/appearance-tabs.tsx
@@ -3,7 +3,13 @@ import { cn } from '@/lib/utils';
 import { LucideIcon, Monitor, Moon, Sun } from 'lucide-react';
 import { HTMLAttributes } from 'react';
 
-export default function AppearanceToggleTab({ className = '', ...props }: HTMLAttributes<HTMLDivElement>) {
+export default function AppearanceToggleTab({
+    className = '',
+    showLabel=true,
+    ...props
+}: HTMLAttributes<HTMLDivElement> & {
+    showLabel?: boolean
+}) {
     const { appearance, updateAppearance } = useAppearance();
 
     const tabs: { value: Appearance; icon: LucideIcon; label: string }[] = [
@@ -26,7 +32,7 @@ export default function AppearanceToggleTab({ className = '', ...props }: HTMLAt
                     )}
                 >
                     <Icon className="-ml-1 h-4 w-4" />
-                    <span className="ml-1.5 text-sm">{label}</span>
+                    { showLabel && <span className="ml-1.5 text-sm">{label}</span> }
                 </button>
             ))}
         </div>
