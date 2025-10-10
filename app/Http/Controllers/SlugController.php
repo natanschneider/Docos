46d21664ac9fc865f5b13a534ffdff762dba5a91
Natãn Schneider <natanschneider@protonmail.com>
2025-10-10 17:35:28 -0300

    *feat* handle URL slugs

---
+++ app/Http/Controllers/SlugController.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\Company;
+use Illuminate\Http\RedirectResponse;
+use Str;
+
+class SlugController extends Controller
+{
+    public function company(string $slug): RedirectResponse|bool
+    {
+        [$title, $companyId] = $this->explodeSlug($slug);
+
+        $company = Company::findOrFail($companyId, ['public_key'])->first();
+
+        $dbTitle = Str::slug($company->name, '-');
+
+        if ($title !== $dbTitle) {
+            return redirect()->route('company', ["$dbTitle-$company->public_key"]);
+        }
+
+        return true;
+    }
+
+    private function explodeSlug(string $slug): array
+    {
+        $parts = array_reverse(explode('-', strrev($slug), 2));
+        return array_map('strrev', $parts);
+    }
+}
