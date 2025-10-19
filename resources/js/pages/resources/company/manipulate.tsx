d63362f73bc1cef4ae281446e2d3d2b559ed76f8
Natãn Schneider <natanschneider@protonmail.com>
2025-10-19 17:47:50 -0300

    *feat* create page to manipulate Companies

---
+++ resources/js/pages/resources/company/manipulate.tsx
@@ -0,0 +1,89 @@
+import CompanyController from '@/actions/App/Http/Controllers/CompanyController';
+import HeadingSmall from '@/components/heading-small';
+import InputError from '@/components/input-error';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Textarea } from '@/components/ui/textarea';
+import AppLayout from '@/layouts/app-layout';
+import ResourcesLayout from '@/layouts/resources/layout';
+import { type BreadcrumbItem } from '@/types';
+import { CompanyNavItems, type companyModel } from '@/types/resources.d';
+import { Transition } from '@headlessui/react';
+import { Form, Head } from '@inertiajs/react';
+import { useRef } from 'react';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Company',
+        href: '',
+    },
+];
+
+export default function Company(company: companyModel | null) {
+    const name = useRef<HTMLInputElement>(null);
+    const description = useRef<HTMLInputElement>(null);
+
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Password settings" />
+
+            <ResourcesLayout title="Settings" description="Manage your profile and account settings" sidebarNavItems={CompanyNavItems}>
+                <div className="space-y-6">
+                    <HeadingSmall title="Update password" description="Ensure your account is using a long, random password to stay secure" />
+
+                    <Form
+                        {...CompanyController.update.form()}
+                        options={{
+                            preserveScroll: true,
+                        }}
+                        resetOnError={['password', 'password_confirmation', 'current_password']}
+                        resetOnSuccess
+                        className="space-y-6"
+                    >
+                        {({ errors, processing, recentlySuccessful }) => (
+                            <>
+                                <div className="grid gap-2">
+                                    <Label htmlFor="current_password">Name</Label>
+
+                                    <Input id="name" ref={name} name="name" type="text" className="mt-1 block w-full" autoComplete="name" />
+
+                                    <InputError message={errors.name} />
+                                </div>
+
+                                <div className="grid gap-2">
+                                    <Label htmlFor="description">Description</Label>
+
+                                    <Textarea
+                                        id="description"
+                                        ref={description}
+                                        name="description"
+                                        className="mt-1 block w-full"
+                                        autoComplete="description"
+                                        placeholder="Company's Description"
+                                    />
+
+                                    <InputError message={errors.description} />
+                                </div>
+
+                                <div className="flex items-center gap-4">
+                                    <Button disabled={processing}>Save company</Button>
+
+                                    <Transition
+                                        show={recentlySuccessful}
+                                        enter="transition ease-in-out"
+                                        enterFrom="opacity-0"
+                                        leave="transition ease-in-out"
+                                        leaveTo="opacity-0"
+                                    >
+                                        <p className="text-sm text-neutral-600">Saved</p>
+                                    </Transition>
+                                </div>
+                            </>
+                        )}
+                    </Form>
+                </div>
+            </ResourcesLayout>
+        </AppLayout>
+    );
+}
