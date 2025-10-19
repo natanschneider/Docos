202a577226599018a21771b36bbeb189d3373d51
Natãn Schneider <natanschneider@protonmail.com>
2025-10-19 17:43:26 -0300

    *feat* create types interfaces for resource pages

---
+++ resources/js/types/resources.d.ts
@@ -0,0 +1,55 @@
+import { appearance } from '@/routes';
+import { edit as editPassword } from '@/routes/password';
+import { edit } from '@/routes/profile';
+import { InertiaLinkProps } from '@inertiajs/react';
+import { LucideIcon } from 'lucide-react';
+import { type ReactNode } from 'react';
+
+export interface companyModel {
+    id: number;
+    name: string;
+    description: string;
+    created_at: string;
+    updated_at: string;
+    public_key: string;
+}
+
+export interface ResourcesLayoutProps {
+    sidebarNavItems: NavItem[];
+    title: string;
+    description: string;
+    children: ReactNode;
+}
+
+export interface NavItem {
+    title: string;
+    href: NonNullable<InertiaLinkProps['href']>;
+    icon?: LucideIcon | null;
+    isActive?: boolean;
+}
+
+export const SettingsNavItems: NavItem[] = [
+    {
+        title: 'Profile',
+        href: edit(),
+        icon: null,
+    },
+    {
+        title: 'Password',
+        href: editPassword(),
+        icon: null,
+    },
+    {
+        title: 'Appearance',
+        href: appearance(),
+        icon: null,
+    },
+];
+
+export const CompanyNavItems: NavItem[] = [
+    {
+        title: 'Create',
+        href: company(),
+        icon: null,
+    },
+];
