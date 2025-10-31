1c21ca37488e1b0f7d98c791ff64967d9fbe5570
Natãn Schneider <natanschneider@protonmail.com>
2025-10-31 15:38:56 -0300

    *feat* get latest Project on DB

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\ProjectRequest;
 use App\Models\Project;
 use App\Repositories\ProjectRepository;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 
 class ProjectController extends Controller
@@ -38,5 +39,14 @@ class ProjectController extends Controller
         Gate::authorize('delete', [Project::findOrFail($request->id), $request]);
 
         return (new ProjectRepository())->destroy($request);
+    }
+
+    public function getLatest(Request $request, ?int $id = null): array
+    {
+        $project = (new ProjectRepository())->getLatest($request, $id);
+        $id = isset($project['id']) ? (int) $project['id'] : null;
+        cookie()->queue('currentProject', $id, 60);
+
+        return $project;
     }
 }
