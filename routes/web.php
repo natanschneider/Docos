a1f7a98287fbbf327ba3cb9494857d9b0ab85a8e
Natan Schneider <natan.schneider@deltasul.com.br>
2026-02-05 14:21:01 -0300

    *feat* query for project and application when any cookie changes

--- routes/web.php
+++ routes/web.php
@@ -35,10 +35,13 @@ Route::middleware(['auth', 'verified'])->group(function () {
         $request->cookies->set('currentCompany', $company);
         $currentProject = (new ProjectRepository())->getLatest($request);
         $currentProject = (is_array($currentProject) && isset($currentProject['id'])) ? $currentProject['id'] : null;
+        $currentApplication = (new ApplicationRepository())->getLatest($request);
+        $currentApplication = (is_array($currentApplication) && isset($currentApplication['id'])) ? $currentApplication['id'] : null;
 
         return Redirect::route('dashboard')
             ->withCookie('currentCompany', $company)
-            ->withCookie('currentProject', $currentProject);
+            ->withCookie('currentProject', $currentProject)
+            ->withCookie('currentApplication', $currentApplication);
     })->name('change-company');
 
     Route::get('change-project/{project}', function (string $project, Request $request) {
@@ -46,15 +49,30 @@ Route::middleware(['auth', 'verified'])->group(function () {
         $currentProject = (new ProjectRepository())->getLatest($request, (int) $project);
         $currentProject = (is_array($currentProject) && isset($currentProject['id'])) ? $currentProject['id'] : null;
 
-        return Redirect::route('application.index')->withCookie('currentProject', $currentProject);
+        $currentApplication = (new ApplicationRepository())->getLatest($request);
+        $currentApplication = (is_array($currentApplication) && isset($currentApplication['id'])) ? $currentApplication['id'] : null;
+
+        return Redirect::route('application.index')
+            ->withCookie('currentProject', $currentProject)
+            ->withCookie('currentApplication', $currentApplication);
     })->name('change-project');
 
-    Route::get('change-application/{application}', function (string $application, Request $request) {
+    Route::get('change-application/{project}/{application}', function (string $project, string $application, Request $request) {
+        $request->cookies->set('currentProject', $project);
         $request->cookies->set('currentApplication', $application);
+
+        $currentProject = (new ProjectRepository())->getLatest($request, (int) $project);
+        $currentProject = (is_array($currentProject) && isset($currentProject['id'])) ? $currentProject['id'] : null;
+
         $currentApplication = (new ApplicationRepository())->getLatest($request, (int) $application);
         $currentApplication = (is_array($currentApplication) && isset($currentApplication['id'])) ? $currentApplication['id'] : null;
 
-        return Redirect::route('screen.index')->withCookie('currentApplication', $currentApplication);
+        $request->cookies->set('currentProject', $project);
+        $request->cookies->set('currentApplication', $application);
+
+        return Redirect::route('screen.index')
+            ->withCookie('currentApplication', $currentApplication)
+            ->withCookie('currentProject', $currentProject);
     })->name('change-application');
 });
 
