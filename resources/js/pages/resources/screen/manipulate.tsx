b30312af629ff91c283307814f48e45ec94d546a
Natan Schneider <natan.schneider@deltasul.com.br>
2026-02-06 16:52:08 -0300

    *feat* add listing of tables and columns being used

--- resources/js/pages/resources/screen/manipulate.tsx
+++ resources/js/pages/resources/screen/manipulate.tsx
@@ -7,11 +7,12 @@ import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/component
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card';
 import AppLayout from '@/layouts/app-layout';
 import ResourcesLayout from '@/layouts/resources/layout';
 import screen from '@/routes/screen';
 import { type BreadcrumbItem, type SharedData } from '@/types';
-import { projectModel, screenModel, ScreenNavItems, tableModel, type applicationModel } from '@/types/resources.d';
+import { columnModel, projectModel, screenModel, ScreenNavItems, tableModel, type applicationModel } from '@/types/resources.d';
 import { Transition } from '@headlessui/react';
 import { Form, Head, usePage } from '@inertiajs/react';
 import { ChevronsUpDown } from 'lucide-react';
@@ -38,8 +39,26 @@ export default function ManipulateScreen({
     const { currentApplication } = usePage<SharedData>().props;
     const [isOpen, setIsOpen] = React.useState(true);
     const name = useRef<HTMLInputElement>(null);
-    const [items, setItems] = React.useState<string[]>(screen !== null ? screen[0]?.columns.map((col) => col.id.toString()) : []);
-    const [selectedTables, setSelectedTables] = React.useState<string[]>(screen !== null ? screen[0]?.columns.map((col) => col.table_id.toString()) : []);
+    const [items, setItems] = React.useState<string[]>(() => {
+        if (!screen || !screen[0]?.columns) return [];
+
+        const uniqueItems = Array.from(new Set(
+            screen[0].columns
+            .map((col) => col.id.toString())
+        ));
+        console.log(uniqueItems);
+        return uniqueItems;
+    });
+    const [selectedTables, setSelectedTables] = React.useState<string[]>(() => {
+        if (!screen || !screen[0]?.columns) return [];
+
+        const uniqueItems = Array.from(new Set(
+            screen[0].columns
+            .map((col) => col.table_id.toString())
+        ));
+
+        return uniqueItems;
+    });
     const [selected, setSelected] = React.useState<string | undefined>(undefined);
 
     const tableArr = tables.reduce(
@@ -50,25 +69,37 @@ export default function ManipulateScreen({
         {} as Record<string, tableModel>,
     );
 
+    const columnArr = tables.reduce((acc, table) => {
+        const columns = table?.columns;
+        columns?.forEach((column) => {
+            acc[column?.id?.toString()] = column;
+        });
+        return acc;
+    }, {} as Record<string, columnModel>);
+
     const columnTable = tables.reduce((acc, table) => {
         const columns = table?.columns;
         columns?.forEach((column) => {
-            if (column.id) {
-                acc[column.id.toString()] = table.id.toString();
-            }
+            acc[column?.id?.toString()] = table?.id?.toString();
         });
         return acc;
     }, {} as Record<string, string>);
 
     const addItem = (item: string) => {
         if (!items?.includes(item)) {
-            setItems([...items??[], item]);
+            setItems([...items, item]);
             if (! selectedTables?.includes(columnTable[item])) {
                 setSelectedTables([...selectedTables??[], columnTable[item]]);
             }
         }
 
         setSelected(undefined);
+    };
+
+    const addTable = (item: string) => {
+        if (! selectedTables?.includes(item)) {
+            setSelectedTables([...selectedTables??[], item]);
+        }
     };
 
     const removeItem = (item: string) => {
@@ -126,10 +157,10 @@ export default function ManipulateScreen({
                                     <InputError message={errors.name} />
                                 </div>
 
-                                <div className="grid gap-2">
-                                    <Collapsible open={isOpen} onOpenChange={setIsOpen} className="flex w-[350px] flex-col gap-2">
+                                <div className='w-full'>
+                                    <Collapsible open={isOpen} onOpenChange={setIsOpen} className="flex w-full flex-col gap-2">
                                         <div className="flex items-center justify-between gap-4 px-4">
-                                            <Label htmlFor="database_id">Databases</Label>
+                                            <Label htmlFor="tables_id">Tables</Label>
                                             <CollapsibleTrigger asChild>
                                                 <Button variant="ghost" size="icon" className="size-8">
                                                     <ChevronsUpDown />
@@ -138,13 +169,13 @@ export default function ManipulateScreen({
                                             </CollapsibleTrigger>
                                         </div>
 
-                                        <Select value={selected} name="database_id" onValueChange={(value) => addItem(value)}>
+                                        <Select value={selected} name="tables_id" onValueChange={(value) => addTable(value)}>
                                             <SelectTrigger className="w-[180px]">
-                                                <SelectValue placeholder="Select an database" />
+                                                <SelectValue placeholder="Select a table" />
                                             </SelectTrigger>
                                             <SelectContent>
                                                 <SelectGroup>
-                                                    <SelectLabel>Databases</SelectLabel>
+                                                    <SelectLabel>Tables</SelectLabel>
                                                     {tables.map((table) => (
                                                         <SelectItem key={table.id} value={table.id.toString()}>
                                                             {table.name}
@@ -154,23 +185,55 @@ export default function ManipulateScreen({
                                             </SelectContent>
                                         </Select>
 
-                                        <InputError message={errors.databases} />
+                                        <InputError message={errors.tables} />
 
-                                        <input type="hidden" name="databases[]" value={items} />
-                                        <CollapsibleContent className="mt-3 flex flex-col gap-2">
-                                            {items?.map((item, index) => (
-                                                <div className="flex rounded-md border px-4 py-2 font-mono text-sm items-center" key={index}>
-                                                    <p className="grow">{tableArr[item]}</p>
-                                                    <Button
-                                                        size="sm"
-                                                        variant="outline"
-                                                        className="min-w-[4rem] text-destructive hover:bg-destructive/10 hover:text-destructive"
-                                                        onClick={() => removeItem(item)}
-                                                    >
-                                                        Delete
-                                                    </Button>
-                                                </div>
+                                        <input type='hidden' name='columns[]' value={items} />
+                                        <CollapsibleContent className='grid gap-2 auto-cols-max grid-flow-col w-full'>
+                                            {selectedTables?.map((table) => (
+                                                <Card key={table}>
+                                                    <CardHeader>
+                                                        <CardTitle>{tableArr[table]?.name}</CardTitle>
+                                                        <CardDescription>
+                                                            <Label htmlFor={`${table}_columns_id`}>Columns</Label>
+                                                            <Select value={selected} name={`${table}_columns_id`} onValueChange={(value) => addItem(value)}>
+                                                                <SelectTrigger className="w-[180px]">
+                                                                    <SelectValue placeholder="Select a column" />
+                                                                </SelectTrigger>
+                                                                <SelectContent>
+                                                                    <SelectGroup>
+                                                                        <SelectLabel>Columns</SelectLabel>
+                                                                        {tableArr[table]?.columns?.map((column) => (
+                                                                            <SelectItem key={column.id} value={column.id.toString()}>
+                                                                                {column.name}
+                                                                            </SelectItem>
+                                                                        ))}
+                                                                    </SelectGroup>
+                                                                </SelectContent>
+                                                            </Select>
+                                                        </CardDescription>
+                                                    </CardHeader>
+                                                    <CardContent>
+                                                        <InputError message={errors[`${table}_columns_id`]} />
+                                                        {items?.map((item, index) => columnTable[item] === table && (
+                                                            <div
+                                                                className="flex rounded-md border px-4 py-2 my-2 font-mono text-sm items-center"
+                                                                key={index}
+                                                            >
+                                                                <p className="grow">{ columnArr[item]?.name }</p>
+                                                                <Button
+                                                                    size="sm"
+                                                                    variant="outline"
+                                                                    className="min-w-[4rem] text-destructive hover:bg-destructive/10 hover:text-destructive"
+                                                                    onClick={() => removeItem(item)}
+                                                                >
+                                                                    Delete
+                                                                </Button>
+                                                            </div>
+                                                        ))}
+                                                    </CardContent>
+                                                </Card>
                                             ))}
+                                            <input type="hidden" name="columns[]" id='columns[]' value={items} />
                                         </CollapsibleContent>
                                     </Collapsible>
                                 </div>
