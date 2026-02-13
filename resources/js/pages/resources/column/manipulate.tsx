95eccb8e7dda8e1435b0f7f75aca47595a59cc3a
Natan Schneider <natan.schneider@deltasul.com.br>
2026-02-13 11:59:00 -0300

    *feat* add contraint selection

--- resources/js/pages/resources/column/manipulate.tsx
+++ resources/js/pages/resources/column/manipulate.tsx
@@ -6,11 +6,13 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible';
+import { ChevronsUpDown } from 'lucide-react';
 import AppLayout from '@/layouts/app-layout';
 import ResourcesLayout from '@/layouts/resources/layout';
 import column from '@/routes/column';
 import { type BreadcrumbItem, type SharedData } from '@/types';
-import { columnModel, ColumnNavItems, tableModel, databaseModel, typeModel } from '@/types/resources.d';
+import { columnModel, ColumnNavItems, tableModel, databaseModel, typeModel, constraintsModel } from '@/types/resources.d';
 import { Transition } from '@headlessui/react';
 import { Form, Head, usePage } from '@inertiajs/react';
 import { useRef } from 'react';
@@ -28,7 +30,8 @@ export default function ManipulateColumn({
     databases,
     primaryKey,
     foreignKey,
-    types
+    types,
+    constraints
 }: {
     column: columnModel[];
     tables: tableModel[];
@@ -36,9 +39,38 @@ export default function ManipulateColumn({
     primaryKey: columnModel[];
     foreignKey: columnModel[];
     types: typeModel[];
+    constraints: constraintsModel[];
 }) {
     const { currentTable } = usePage<SharedData>().props;
     const name = useRef<HTMLInputElement>(null);
+    const [isConstraintOpen, setIsConstraintOpen] = React.useState(true);
+    const [constraint, setConstraint] = React.useState<string[]>(column !== null && column[0]?.constraints
+        ? column[0]?.constraints?.map((constraint) => constraint.id.toString())
+        : []
+    );
+    const [selectedConstraint, setSelectedConstraint] = React.useState<string | undefined>(undefined);
+
+    const constraintArr = constraints.reduce(
+        (acc, constraint) => {
+            acc[constraint.id] = constraint.name;
+            return acc;
+        },
+        {} as Record<string, string>,
+    );
+
+
+    const addConstraint = (item: string) => {
+        if (!constraint.includes(item)) {
+            setConstraint([...constraint, item]);
+        }
+
+        setSelectedConstraint('');
+    };
+
+    const removeConstraint = (item: string) => {
+        setConstraint(constraint.filter((i) => i !== item));
+        setSelectedConstraint('');
+    };
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
@@ -110,6 +142,55 @@ export default function ManipulateColumn({
                                     </Select>
 
                                     <InputError message={errors.type_id} />
+                                </div>
+
+                                <div className="grid gap-2">
+                                    <Collapsible open={isConstraintOpen} onOpenChange={setIsConstraintOpen} className="flex w-[350px] flex-col gap-2">
+                                        <div className="flex items-center justify-between gap-4 px-4">
+                                            <Label htmlFor="constraints_id">Constraints</Label>
+                                            <CollapsibleTrigger asChild>
+                                                <Button variant="ghost" size="icon" className="size-8">
+                                                    <ChevronsUpDown />
+                                                    <span className="sr-only">Toggle</span>
+                                                </Button>
+                                            </CollapsibleTrigger>
+                                        </div>
+
+                                        <Select value={selectedConstraint} name="database_id" onValueChange={(value) => addConstraint(value)}>
+                                            <SelectTrigger className="w-[180px]">
+                                                <SelectValue placeholder="Select an constraint" />
+                                            </SelectTrigger>
+                                            <SelectContent>
+                                                <SelectGroup>
+                                                    <SelectLabel>Constraints</SelectLabel>
+                                                    {constraints.map((constraint) => (
+                                                        <SelectItem key={constraint.id} value={constraint.id.toString()}>
+                                                            {constraint.name}
+                                                        </SelectItem>
+                                                    ))}
+                                                </SelectGroup>
+                                            </SelectContent>
+                                        </Select>
+
+                                        <InputError message={errors.constraints} />
+
+                                        <input type="hidden" name="databases[]" value={constraint} />
+                                        <CollapsibleContent className="mt-3 flex flex-col gap-2">
+                                            {constraint.map((item, index) => (
+                                                <div className="flex rounded-md border px-4 py-2 font-mono text-sm items-center" key={index}>
+                                                    <p className="grow">{constraintArr[item]}</p>
+                                                    <Button
+                                                        size="sm"
+                                                        variant="outline"
+                                                        className="min-w-[4rem] text-destructive hover:bg-destructive/10 hover:text-destructive"
+                                                        onClick={() => removeConstraint(item)}
+                                                    >
+                                                        Delete
+                                                    </Button>
+                                                </div>
+                                            ))}
+                                        </CollapsibleContent>
+                                    </Collapsible>
                                 </div>
 
                                 <div className="flex items-center gap-4">
