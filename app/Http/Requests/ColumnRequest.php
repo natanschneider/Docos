53861b4e8217fede7b062f82d9b7968226ff7f74
Natan Schneider <natan.schneider@deltasul.com.br>
2026-02-19 16:55:02 -0300

    *fix* modify request rules because of error "Array to string conversion"

--- app/Http/Requests/ColumnRequest.php
+++ app/Http/Requests/ColumnRequest.php
@@ -20,9 +20,14 @@ class ColumnRequest extends FormRequest
                 'name' => ['required', 'string', 'max:255'],
                 'table_id' => ['required', 'int', 'exists:tables,id'],
                 'type_id' => ['required', 'int', 'exists:types,id'],
-                'related_columns' => ['array:fk,pk', 'exists:columns,id'],
                 'constraints' => ['array', 'exists:constraints,id'],
                 'indexed' => ['boolean'],
+
+                'related_columns' => ['array'],
+                'related_columns.pk' => ['array'],
+                'related_columns.pk.*' => ['nullable', 'exists:columns,id'],
+                'related_columns.fk' => ['array'],
+                'related_columns.fk.*' => ['nullable', 'exists:columns,id'],
             ],
             'GET' => [
                 'id' => ['required_if:table_id,null', 'int', 'exists:columns,id'],
@@ -33,11 +38,21 @@ class ColumnRequest extends FormRequest
                 'name' => ['string', 'max:255'],
                 'table_id' => ['int', 'exists:tables,id'],
                 'type_id' => ['int', 'exists:types,id'],
-                'related_columns' => ['array:fk,pk', 'exists:columns,id'],
                 'constraints' => ['array', 'exists:constraints,id'],
-                'detach_related_columns' => ['array:fk,pk', 'exists:columns,id'],
                 'detach_constraints' => ['array', 'exists:constraints,id'],
                 'indexed' => ['boolean'],
+
+                'related_columns' => ['array'],
+                'related_columns.pk' => ['array'],
+                'related_columns.pk.*' => ['nullable', 'exists:columns,id'],
+                'related_columns.fk' => ['array'],
+                'related_columns.fk.*' => ['nullable', 'exists:columns,id'],
+
+                'detach_related_columns' => ['array'],
+                'detach_related_columns.pk' => ['array'],
+                'detach_related_columns.pk.*' => ['nullable', 'exists:columns,id'],
+                'detach_related_columns.fk' => ['array'],
+                'detach_related_columns.fk.*' => ['nullable', 'exists:columns,id'],
             ],
             'DELETE' => [
                 'id' => ['required', 'int', 'exists:columns,id'],
@@ -48,10 +63,19 @@ class ColumnRequest extends FormRequest
                 'table_id' => ['int', 'exists:tables,id'],
                 'type_id' => ['int', 'exists:types,id'],
                 'constraints' => ['array', 'exists:constraints,id'],
-                'related_columns' => ['array:fk,pk', 'exists:columns,id'],
-                'detach_constraints' => ['array', 'exists:constraints,id'],
-                'detach_related_columns' => ['array:fk,pk', 'exists:columns,id'],
                 'indexed' => ['boolean'],
+
+                'related_columns' => ['array'],
+                'related_columns.pk' => ['array'],
+                'related_columns.pk.*' => ['nullable', 'exists:columns,id'],
+                'related_columns.fk' => ['array'],
+                'related_columns.fk.*' => ['nullable', 'exists:columns,id'],
+
+                'detach_related_columns' => ['array'],
+                'detach_related_columns.pk' => ['array'],
+                'detach_related_columns.pk.*' => ['nullable', 'exists:columns,id'],
+                'detach_related_columns.fk' => ['array'],
+                'detach_related_columns.fk.*' => ['nullable', 'exists:columns,id'],
             ]
         };
     }
