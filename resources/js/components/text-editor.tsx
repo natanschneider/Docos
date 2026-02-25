43ca412c00377c411c10b062efea26f8f37317b1
Natan Schneider <natan.schneider@deltasul.com.br>
2026-02-25 17:14:31 -0300

    *fix* text editor toolbar

--- resources/js/components/text-editor.tsx
+++ resources/js/components/text-editor.tsx
@@ -1,76 +1,94 @@
-import {
-    MDXEditor,
-    headingsPlugin,
-    listsPlugin,
-    quotePlugin,
-    thematicBreakPlugin,
-    markdownShortcutPlugin,
-    UndoRedo,
-    BoldItalicUnderlineToggles,
-    toolbarPlugin,
-    BlockTypeSelect,
-    linkPlugin,
-    linkDialogPlugin,
-    InsertTable,
-    codeMirrorPlugin,
-    ConditionalContents,
-    ChangeCodeMirrorLanguage,
-    ShowSandpackInfo,
-    InsertCodeBlock,
-    InsertSandpack,
-    type MDXEditorMethods,
-    type MDXEditorProps
-} from '@mdxeditor/editor';
-import '@mdxeditor/editor/style.css'
-import type { ForwardedRef } from 'react';
+'use client';
 
-export default function TextEditor ({
+import {
+    AdmonitionDirectiveDescriptor,
+    codeBlockPlugin,
+    codeMirrorPlugin,
+    diffSourcePlugin,
+    directivesPlugin,
+    frontmatterPlugin,
+    headingsPlugin,
+    imagePlugin,
+    KitchenSinkToolbar,
+    linkDialogPlugin,
+    linkPlugin,
+    listsPlugin,
+    markdownShortcutPlugin,
+    MDXEditor,
+    MDXEditorMethods,
+    MDXEditorProps,
+    quotePlugin,
+    SandpackConfig,
+    sandpackPlugin,
+    tablePlugin,
+    thematicBreakPlugin,
+    toolbarPlugin,
+} from '@mdxeditor/editor';
+import '@mdxeditor/editor/style.css';
+import { ForwardedRef } from 'react';
+
+const defaultSnippetContent = `
+export default function App() {
+    return (
+        <div className="App">
+            <h1>Hello CodeSandbox</h1>
+            <h2>Start editing to see some magic happen!</h2>
+        </div>
+    );
+}
+`.trim();
+
+const reactSandpackConfig: SandpackConfig = {
+    defaultPreset: 'react',
+    presets: [
+        {
+            label: 'React',
+            name: 'react',
+            meta: 'live',
+            sandpackTemplate: 'react',
+            sandpackTheme: 'light',
+            snippetFileName: '/App.js',
+            snippetLanguage: 'jsx',
+            initialSnippetContent: defaultSnippetContent,
+        },
+    ],
+};
+
+const allPlugins = (diffMarkdown: string) => [
+    toolbarPlugin({ toolbarContents: () => <KitchenSinkToolbar /> }),
+    listsPlugin(),
+    quotePlugin(),
+    headingsPlugin(),
+    linkPlugin(),
+    linkDialogPlugin(),
+    imagePlugin({ imageUploadHandler: async () => '/sample-image.png' }),
+    tablePlugin(),
+    thematicBreakPlugin(),
+    frontmatterPlugin(),
+    codeBlockPlugin({ defaultCodeBlockLanguage: 'txt' }),
+    sandpackPlugin({ sandpackConfig: reactSandpackConfig }),
+    codeMirrorPlugin({ codeBlockLanguages: { js: 'JavaScript', css: 'CSS', txt: 'text', tsx: 'TypeScript' } }),
+    directivesPlugin({ directiveDescriptors: [AdmonitionDirectiveDescriptor] }),
+    diffSourcePlugin({ viewMode: 'rich-text', diffMarkdown }),
+    markdownShortcutPlugin(),
+];
+
+export default function TextEditor({
+    markdown,
     editorRef,
     ...props
 }: {
-    editorRef: ForwardedRef<MDXEditorMethods> | null
-} & MDXEditorProps
-) {
+    markdown: string,
+    editorRef: ForwardedRef<MDXEditorMethods>|null
+} & MDXEditorProps) {
     return (
-        <div className="w-full h-full">
-            <MDXEditor
-                {...props}
-                ref={editorRef}
-                plugins={[
-                    headingsPlugin(),
-                    listsPlugin(),
-                    quotePlugin(),
-                    thematicBreakPlugin(),
-                    markdownShortcutPlugin(),
-                    linkPlugin(),
-                    linkDialogPlugin(),
-                    codeMirrorPlugin(),
-                    toolbarPlugin({
-                        toolbarContents: () => (
-                            <>
-                                <UndoRedo />
-                                <BoldItalicUnderlineToggles />
-                                <BlockTypeSelect />
-                                <InsertTable />
-                                <ConditionalContents
-                                    options={[
-                                        { when: (editor) => editor?.editorType === 'codeblock', contents: () => <ChangeCodeMirrorLanguage /> },
-                                        { when: (editor) => editor?.editorType === 'sandpack', contents: () => <ShowSandpackInfo /> },
-                                        {
-                                            fallback: () => (
-                                                <>
-                                                <InsertCodeBlock />
-                                                <InsertSandpack />
-                                                </>
-                                            )
-                                        }
-                                    ]}
-                                />
-                            </>
-                        )
-                    })
-                ]}
-            />
-        </div>
-    )
+        <MDXEditor
+            ref={editorRef}
+            markdown={markdown}
+            {...props}
+            className="full-demo-mdxeditor"
+            contentEditableClassName="prose max-w-full font-sans"
+            plugins={allPlugins(markdown)}
+        />
+    );
 }
