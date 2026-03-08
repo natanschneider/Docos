c8b78495540223a773088613a6699063dcb00a40
Natan Schneider <natan.schneider@protonmail.com>
2026-03-07 22:46:13 -0300

    *fix* reformat | toolbar

--- resources/js/components/text-editor.tsx
+++ resources/js/components/text-editor.tsx
@@ -1,32 +1,39 @@
-'use client';
+"use client";
 
+import { useAppearance } from "@/hooks/use-appearance";
 import {
-    AdmonitionDirectiveDescriptor,
-    codeBlockPlugin,
-    codeMirrorPlugin,
-    diffSourcePlugin,
-    directivesPlugin,
-    frontmatterPlugin,
-    headingsPlugin,
-    imagePlugin,
-    KitchenSinkToolbar,
-    linkDialogPlugin,
-    linkPlugin,
-    listsPlugin,
-    markdownShortcutPlugin,
-    MDXEditor,
-    MDXEditorMethods,
-    MDXEditorProps,
-    quotePlugin,
-    SandpackConfig,
-    sandpackPlugin,
-    tablePlugin,
-    thematicBreakPlugin,
-    toolbarPlugin,
-} from '@mdxeditor/editor';
-import '@mdxeditor/editor/style.css';
-import { ForwardedRef } from 'react';
-import { useAppearance } from '@/hooks/use-appearance';
+	AdmonitionDirectiveDescriptor,
+	BlockTypeSelect,
+	BoldItalicUnderlineToggles,
+	codeBlockPlugin,
+	codeMirrorPlugin,
+	CodeToggle,
+	diffSourcePlugin,
+	directivesPlugin,
+	frontmatterPlugin,
+	headingsPlugin,
+	imagePlugin,
+	InsertCodeBlock,
+	InsertTable,
+	InsertThematicBreak,
+	linkDialogPlugin,
+	linkPlugin,
+	listsPlugin,
+	ListsToggle,
+	markdownShortcutPlugin,
+	MDXEditor,
+	MDXEditorMethods,
+	MDXEditorProps,
+	quotePlugin,
+	SandpackConfig,
+	sandpackPlugin,
+	tablePlugin,
+	thematicBreakPlugin,
+	toolbarPlugin,
+	UndoRedo,
+} from "@mdxeditor/editor";
+import "@mdxeditor/editor/style.css";
+import { ForwardedRef } from "react";
 
 const defaultSnippetContent = `
 export default function App() {
@@ -40,57 +47,77 @@ export default function App() {
 `.trim();
 
 const reactSandpackConfig: SandpackConfig = {
-    defaultPreset: 'react',
-    presets: [
-        {
-            label: 'React',
-            name: 'react',
-            meta: 'live',
-            sandpackTemplate: 'react',
-            sandpackTheme: 'light',
-            snippetFileName: '/App.js',
-            snippetLanguage: 'jsx',
-            initialSnippetContent: defaultSnippetContent,
-        },
-    ],
+	defaultPreset: "react",
+	presets: [
+		{
+			label: "React",
+			name: "react",
+			meta: "live",
+			sandpackTemplate: "react",
+			sandpackTheme: "light",
+			snippetFileName: "/App.js",
+			snippetLanguage: "jsx",
+			initialSnippetContent: defaultSnippetContent,
+		},
+	],
 };
 
 const allPlugins = (diffMarkdown: string) => [
-    toolbarPlugin({ toolbarContents: () => <KitchenSinkToolbar /> }),
-    listsPlugin(),
-    quotePlugin(),
-    headingsPlugin(),
-    linkPlugin(),
-    linkDialogPlugin(),
-    imagePlugin({ imageUploadHandler: async () => '/sample-image.png' }),
-    tablePlugin(),
-    thematicBreakPlugin(),
-    frontmatterPlugin(),
-    codeBlockPlugin({ defaultCodeBlockLanguage: 'txt' }),
-    sandpackPlugin({ sandpackConfig: reactSandpackConfig }),
-    codeMirrorPlugin({ codeBlockLanguages: { js: 'JavaScript', css: 'CSS', txt: 'text', tsx: 'TypeScript' } }),
-    directivesPlugin({ directiveDescriptors: [AdmonitionDirectiveDescriptor] }),
-    diffSourcePlugin({ viewMode: 'rich-text', diffMarkdown }),
-    markdownShortcutPlugin(),
+	toolbarPlugin({
+		toolbarContents: () => (
+			<>
+				<BlockTypeSelect />
+				<BoldItalicUnderlineToggles />
+				<CodeToggle />
+				<InsertCodeBlock />
+				<InsertThematicBreak />
+				<InsertTable />
+				<ListsToggle />
+				<UndoRedo />
+			</>
+		),
+	}),
+	listsPlugin(),
+	quotePlugin(),
+	headingsPlugin(),
+	linkPlugin(),
+	linkDialogPlugin(),
+	imagePlugin({ imageUploadHandler: async () => "/sample-image.png" }),
+	tablePlugin(),
+	thematicBreakPlugin(),
+	frontmatterPlugin(),
+	codeBlockPlugin({ defaultCodeBlockLanguage: "txt" }),
+	sandpackPlugin({ sandpackConfig: reactSandpackConfig }),
+	codeMirrorPlugin({
+		codeBlockLanguages: {
+			js: "JavaScript",
+			css: "CSS",
+			txt: "text",
+			tsx: "TypeScript",
+		},
+	}),
+	directivesPlugin({ directiveDescriptors: [AdmonitionDirectiveDescriptor] }),
+	diffSourcePlugin({ viewMode: "rich-text", diffMarkdown }),
+	markdownShortcutPlugin(),
 ];
 
 export default function TextEditor({
-    markdown,
-    editorRef,
-    ...props
+	markdown,
+	editorRef,
+	...props
 }: {
-    markdown: string,
-    editorRef: ForwardedRef<MDXEditorMethods>|null
+	markdown: string;
+	editorRef: ForwardedRef<MDXEditorMethods> | null;
 } & MDXEditorProps) {
-    const { appearance } = useAppearance();
-    return (
-        <MDXEditor
-            ref={editorRef}
-            markdown={markdown}
-            {...props}
-            className={`prose max-w-full font-sans ${appearance === 'dark' || appearance === 'system' ? 'dark-theme mdxeditor' : 'mdxeditor'}`}
-            contentEditableClassName="prose max-w-full font-sans"
-            plugins={allPlugins(markdown)}
-        />
-    );
+	const { appearance } = useAppearance();
+	return (
+		<MDXEditor
+			ref={editorRef}
+			markdown={markdown}
+			{...props}
+			className={`prose max-w-full font-sans ${appearance === "dark" || appearance === "system" ? "dark-theme mdxeditor" : "mdxeditor"}`}
+			contentEditableClassName="prose max-w-full font-sans"
+			plugins={allPlugins(markdown)}
+		/>
+	);
 }
